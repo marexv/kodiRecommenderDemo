4142928d1938c551c3aa1bc72231e48b7ed00ba9
Marko <marko.prcac@hotmail.com>
2018-03-23 01:48:48 +0100

    front page content, look and feel

--- main/views.py
+++ main/views.py
@@ -11,46 +11,6 @@ from main.models import Board, Movie
 from main.forms import movieForm
 
 # Create your views here.
-
-# Algorithm
-# 
-# steps = 1
-# movie_id = 1
-# 
-# # Get first movie:
-# try:
-#     movie = Movie.objects.get(movie_id=movie_id)
-# except Exception:
-#     movie = None
-# 
-# # Choose board with biggest number of movies for starting board.
-# boards = Board.objects.annotate(Count('movies')).filter(movies=movie).order_by('-movies__count')
-# starting_node = boards[0]
-# 
-# board_node = starting_node
-# # Random walk:
-# movies = {}
-# for step in range(steps):
-#     #
-#     movies_in_board = board_node.movies.all()
-#     next_movie_index = randrange(0, len(movies_in_board))
-#     movie_node = movies_in_board[next_movie_index]
-#     try:
-#         movies[movie_node] = movies[movie_node] +1
-#     except Exception as e:
-#         print(str(e))
-#         movies[movie_node] = 1
-#     #
-#     boards_containging_movie = movie_node.boards.all()
-#     next_board_index = randrange(0, len(boards_containging_movie))
-#     board_node = boards_containging_movie[next_board_index]
-#     #
-#     if uniform(0,1) < 0.5:
-#         board_node = starting_node
-# 
-# sorted_movies = sorted(movies.items(), key=operator.itemgetter(1), reverse=True)
-
-
 def get_recommendations(movie, steps=1000, alpha=0.5):
     boards = Board.objects.annotate(Count('movies')).filter(movies=movie).order_by('-movies__count')
     starting_node = boards[0]
@@ -80,7 +40,6 @@ def get_recommendations(movie, steps=1000, alpha=0.5):
     sorted_movies = sorted(movies.items(), key=operator.itemgetter(1), reverse=True)
     return sorted_movies
 
-# a = dict(Counter(movies))
 
 def autocomplete(request):
     """Autocomplete fbid field in form used to send transaction notification.
